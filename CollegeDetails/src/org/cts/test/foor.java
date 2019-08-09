f6e9d4d97b2d5c1655f1b36f733210b39ff60205
Anwaribu0070 <juzlyktat.anwar@gmail.com>
2019-08-09 19:08:29 +0530

    message

---
+++ CollegeDetails/src/org/cts/test/foor.java
@@ -0,0 +1,14 @@
+package org.cts.test;
+
+public class foor 
+
+//(initialization condition checking increment decrement )
+{
+	public static void main(String[] args) {
+		for(int i=0; i<=10; i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
